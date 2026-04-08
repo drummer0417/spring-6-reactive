40402e6bada35ebbe4c0d19374f2ec7d2996264c
Hans <drummer0417@gmail.com>
2026-04-08 11:52:14 +0200

    Second commit

--- src/main/java/nl/androidappfactory/spring6reactive/repositories/PersonRepository.java
+++ src/main/java/nl/androidappfactory/spring6reactive/repositories/PersonRepository.java
@@ -1,4 +1,12 @@
 package nl.androidappfactory.spring6reactive.repositories;
 
+import nl.androidappfactory.spring6reactive.domain.Person;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface PersonRepository {
+
+    Mono<Person> getById(Integer id);
+
+    Flux<Person> findAll();
 }
--- src/test/java/nl/androidappfactory/spring6reactive/repositories/PersonRepositoryImplTest.java
+++ src/test/java/nl/androidappfactory/spring6reactive/repositories/PersonRepositoryImplTest.java
@@ -1,4 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
+package nl.androidappfactory.spring6reactive.repositories;
+
+import nl.androidappfactory.spring6reactive.domain.Person;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
 class PersonRepositoryImplTest {
-  
+
+    private final PersonRepositoryImpl personRepository = new PersonRepositoryImpl();
+
+    @Test
+    void testMonoByIdBlock() {
+        Mono<Person> personMono = personRepository.getById(1);
+        Person person = personMono.block();
+
+        System.out.println(person);
+    }
+
+    @Test
+    void testMonoByIdSubscribe(){
+        Mono<Person> personMono = personRepository.getById(3);
+
+        personMono.subscribe(System.out::println);
+    }
 }
